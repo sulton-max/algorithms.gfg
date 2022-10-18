a1a841047501bdbb3c558a21e680ea86354e228d
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2022-10-19 02:15:26 +0500

    Added linear search

---
+++ algorithms.gfg/Program.cs
@@ -0,0 +1,13 @@
+﻿using gfg_algorithms.Searching;
+
+
+var test1 = new[] { 1, 6, 7, 4, 6 };
+var test2 = new[] { 11, 67, 27, 74, 86 };
+
+// Linear search
+Console.WriteLine(LinearSearch.Search(test1, 27));
+Console.WriteLine(LinearSearch.Search(test2, 27));
+
+// Recursive linear search
+Console.WriteLine(LinearSearch.SearchRecursively(test1, 27));
+Console.WriteLine(LinearSearch.SearchRecursively(test2, 27));
---
+++ algorithms.gfg/Searching/LinearSearch.cs
@@ -0,0 +1,33 @@
+﻿namespace gfg_algorithms.Searching
+{
+    public static class LinearSearch
+    {
+        public static int Search(int[] array, int value)
+        {
+            if (array == null || array.Length == 0)
+                return -1;
+
+            for (int index = 0; index < array.Length; index++)
+                if (array[index] == value)
+                    return index;
+
+            return -1;
+        }
+
+        public static int SearchRecursively(int[] array, int value, int? index = default)
+        {
+            if (array == null || array.Length == 0)
+                return -1;
+
+            if (!index.HasValue)
+                index = array.Length - 1;
+
+            if (index == 0)
+                return -1;
+            else if (array[index.Value - 1] == value)
+                return index.Value - 1;
+            else 
+                return SearchRecursively(array, value, index - 1);
+        }
+    }
+}
